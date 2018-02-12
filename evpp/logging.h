61493cd1692c304511db95ce88467103a4c9d999
NickYang <362148418@qq.com>
2018-02-12 13:55:33 +0800

    remove console log

--- evpp/logging.h
+++ evpp/logging.h
@@ -4,19 +4,30 @@
 
 #ifdef __cplusplus
 
+#ifdef EVPP_OPEN_LOG
+
 #define LOG_TRACE std::cout << __FILE__ << ":" << __LINE__ << " "
 #define LOG_DEBUG std::cout << __FILE__ << ":" << __LINE__ << " "
 #define LOG_INFO  std::cout << __FILE__ << ":" << __LINE__ << " "
 #define LOG_WARN  std::cout << __FILE__ << ":" << __LINE__ << " "
 #define LOG_ERROR std::cout << __FILE__ << ":" << __LINE__ << " "
 #define LOG_FATAL std::cout << __FILE__ << ":" << __LINE__ << " "
-#define CHECK_NOTnullptr(val) LOG_ERROR << "'" #val "' Must be non nullptr";
-#ifdef _DEBUG
 #define DLOG_TRACE LOG_TRACE
 #define DLOG_WARN LOG_WARN
+#define CHECK_NOTnullptr(val) LOG_ERROR << "'" #val "' Must be non nullptr";
+
 #else
-#define DLOG_TRACE if (false) LOG_TRACE
-#define DLOG_WARN if (false) LOG_WARN
+
+#define LOG_TRACE if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define LOG_DEBUG if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define LOG_INFO  if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define LOG_WARN  if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define LOG_ERROR if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define LOG_FATAL if(false) std::cout << __FILE__ << ":" << __LINE__ << " "
+#define DLOG_TRACE LOG_TRACE
+#define DLOG_WARN LOG_WARN
+#define CHECK_NOTnullptr(val) LOG_ERROR << "'" #val "' Must be non nullptr";
+
 #endif
 
 #endif
